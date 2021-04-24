3c5c8dfa02966cbfb67aac9ff897a1c79365b0ec
Niklas Jansson <niklas.jansson@nightflyers.org>
2021-04-24 17:38:26 +0200

    added an install script for installing all needed files

---
+++ scripts/install_all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BIN=../FFXIVAPP.Plugin.TeastParse/bin/Debug/netstandard2.0
+DEST=../../ffxivapp/FFXIVAPP.Client/bin/Debug/netcoreapp3.0/Plugins/FFXIVAPP.Plugin.TeastParse
+
+
+./install.sh $BIN/FFXIVAPP.Plugin.TeastParse.dll $DEST || exit 1
+./install.sh $BIN/FFXIVAPP.Plugin.TeastParse.pdb $DEST || exit 2
+./install.sh $BIN/PluginInfo.xml $DEST || exit 3
+./install.sh ../FFXIVAPP.Plugin.TeastParse/Logo.png $DEST || exit 4
